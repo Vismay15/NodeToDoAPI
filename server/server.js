54b2feca390ab2a7ca968050ecbaaca09dddba8f
Vismay15 <vis.patel15@gmail.com>
2018-10-23 16:29:44 +0530

    Added fetchByID GET API

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //require the custom code writtten by us 
 var {mongoose} = require('./db/mongoose');
@@ -31,6 +32,21 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/fetchByID/:id',(req,res)=>{
+  var id = req.params.id;
+  if (!ObjectID.isValid(id) || id == null || id == undefined || id.toString().trim() == '') {
+    return res.status(400).send();
+  } else {
+    Todo.findById(id,(error,todo)=>{
+      if (error) {
+        return res.status(400).send();
+      } else {
+        res.send({todo});
+      }
+    })
+  }
+})
 
 //start the app on port 3000
 app.listen(3000, () => {
